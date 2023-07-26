e0a7ce455ed7b50baf0b0372ea270ec2dddf5782
Yuanyin Zhang <isyuanyin@gmail.com>
2023-07-26 19:40:14 +0800

    update

--- C++/214.cpp
+++ C++/214.cpp
@@ -3,6 +3,25 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-
+        int n = s.size();
+        vector<int> next(n, -1);
+        for (int l = 0, r = 1; r < n; ++l, ++r) {
+            next[r] = l;
+            if (s[l] == s[r]) {
+                while ((next[r] = next[next[r]]) >= 0 && s[next[r]] == s[r])
+                    ;
+            } else {
+                while ((l = next[l]) >= 0 && s[l] != s[r])
+                    ;
+            }
+        }
+        int j = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            while (j >= 0 && s[j] != s[i]) j = next[j];
+            j += 1;
+        }
+        auto add = j == n ? "" : s.substr(j);
+        reverse(add.begin(), add.end());
+        return add + s;
     }
 };
--- src/_459.rs
+++ src/_459.rs
@@ -2,6 +2,33 @@ use crate::leetcode::Solution;
 
 impl Solution {
     pub fn repeated_substring_pattern(s: String) -> bool {
-        false
+        let li = s.as_bytes().into_iter().collect::<Vec<_>>();
+        let mut next = vec![-1 as i32; li.len()];
+        let (mut l, mut r) = (0, 1);
+        while r < li.len() {
+            next[r as usize] = l;
+            if *li[l as usize] == *li[r as usize] {
+                next[r] = next[next[r as usize] as usize];
+                while next[r] >= 0 && *li[next[r] as usize] == *li[r as usize] {
+                    next[r] = next[next[r as usize] as usize];
+                }
+            } else {
+                l = next[l as usize];
+                while l >= 0 && *li[l as usize] != *li[r] {
+                    l = next[l as usize];
+                }
+            }
+            l += 1;
+            r += 1;
+        }
+        if l <= 0 {
+            return false;
+        }
+        for i in 0..(r as i32 - l) {
+            if *li[i as usize] != *li[(i + l) as usize] {
+                return false;
+            }
+        }
+        true
     }
 }
